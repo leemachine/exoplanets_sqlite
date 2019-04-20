713c3aa2efa6e91af8177b7f1667bf6c27e577ab
Brandon Ess <brandon.ess@gmail.com>
2019-04-20 11:21:57 -0700

    Update exoplanets.sql

--- exoplanets.sql
+++ exoplanets.sql
@@ -1,62 +1,48 @@
-CREATE TABLE confirmed ( 
-	confirmed            boolean NOT NULL DEFAULT 0 ,
-	conf_date            date ,
-	conf_method          text ,
-	CONSTRAINT Pk_confirmed_confirmed PRIMARY KEY ( confirmed )
- );
+CREATE TABLE planet (
+        p_name text NOT NULL ,
+        s_name text ,
+        CONSTRAINT Pk_planet_p_name PRIMARY KEY ( p_name ) ,
+        FOREIGN KEY ( s_name ) REFERENCES star( s_name )
+);
 
-CREATE TABLE constellation ( 
-	constellation        text NOT NULL ,
-	brightest_star       text ,
-	CONSTRAINT Pk_constellation_constellation PRIMARY KEY ( constellation )
- );
+CREATE TABLE star (
+        s_name text ,
+        CONSTRAINT Pk_star_s_name PRIMARY KEY ( s_name )
+);
 
-CREATE TABLE discovery ( 
-	disc_date            date NOT NULL ,
-	disc_site            text ,
-	disc_method          text ,
-	notes                text ,
-	CONSTRAINT Pk_discovery_disc_date PRIMARY KEY ( disc_date )
- );
+CREATE TABLE discovery (
+        disc_method    text NOT NULL ,
+        p_name         text ,
+        disc_date      date ,
+        disc_site      text ,
+        disc_confirmed boolean  DEFAULT 0 ,
+        CONSTRAINT Pk_discovery_p_name PRIMARY KEY ( p_name ) ,
+        FOREIGN KEY ( p_name ) REFERENCES planet( p_name )
+);
 
-CREATE TABLE planet_type ( 
-	p_type               text NOT NULL ,
-	discription          text ,
-	CONSTRAINT Pk_planet_type_p_type PRIMARY KEY ( p_type )
- );
+CREATE INDEX Pk_discovery_disc_method ON discovery ( disc_method );
 
-CREATE TABLE star_type ( 
-	s_type               text NOT NULL  ,
-	color                text ,
-	description          text ,
-	CONSTRAINT Pk_star_type_s_type PRIMARY KEY ( s_type )
- );
+CREATE TABLE planet_type (
+        p_type      text NOT NULL ,
+        p_name      text ,
+        description text ,
+        radius      integer ,
+        d2s         integer ,
+        d2e         integer ,
+        CONSTRAINT Pk_planet_type_p_type PRIMARY KEY ( p_type ) ,
+        FOREIGN KEY ( p_name ) REFERENCES planet( p_name )
+);
 
-CREATE TABLE star ( 
-	s_name               text NOT NULL ,
-	s_type               text ,
-	mass                 integer ,
-	temperature          integer ,
-	constellation        text ,
-	CONSTRAINT Pk_star_s_name PRIMARY KEY ( s_name ),
-	FOREIGN KEY ( s_type ) REFERENCES star_type( s_type )  ,
-	FOREIGN KEY ( constellation ) REFERENCES constellation( constellation )  
- );
+CREATE TABLE star_type (
+        s_type        text NOT NULL ,
+        s_name        text ,
+        color         text ,
+        description   text ,
+        mass          integer ,
+        temperature   integer ,
+        constellation text ,
+        CONSTRAINT Pk_star_type_s_type PRIMARY KEY ( s_type ) ,
+        FOREIGN KEY ( s_name ) REFERENCES star( s_name )
+);
 
-CREATE TABLE planets ( 
-	p_name               text NOT NULL ,
-	p_type               text ,
-	radius               integer ,
-	mass                 integer ,
-	d2s                  integer ,
-	d2e                  integer ,
-	star                 text ,
-	disc_date            date ,
-	confirmed            boolean NOT NULL DEFAULT 0 ,
-	CONSTRAINT Pk_planets_p_name PRIMARY KEY ( p_name ),
-	FOREIGN KEY ( star ) REFERENCES star( s_name ) ,
-	FOREIGN KEY ( p_type ) REFERENCES planet_type( p_type ) ,
-	FOREIGN KEY ( disc_date ) REFERENCES discovery( disc_date ) ,
-	FOREIGN KEY ( confirmed ) REFERENCES confirmed( confirmed ) 
- );
 
